2796c575b207b34d5833ee24e69011e6660132ba
Ayush Khot <98622875+fresleven@users.noreply.github.com>
2025-06-05 15:31:45 -0500

    Update get_jetclass.sh
    Change range to download all of JetClass

--- datasets/jetclass/get_jetclass.sh
+++ datasets/jetclass/get_jetclass.sh
@@ -4,7 +4,7 @@ mkdir -p raw/val
 mkdir -p raw/test
 mkdir -p processed
 wget -O raw/test/JetClass_Pythia_test_20M.tar "https://zenodo.org/records/6619768/files/JetClass_Pythia_test_20M.tar?download=1"
-for i in {0..0}; do
+for i in {0..9}; do
   wget -O "raw/train/JetClass_Pythia_train_100M_part${i}.tar" "https://zenodo.org/records/6619768/files/JetClass_Pythia_train_100M_part${i}.tar?download=1"
 done
 wget -O raw/val/JetClass_Pythia_val_5M.tar "https://zenodo.org/records/6619768/files/JetClass_Pythia_val_5M.tar?download=1"
